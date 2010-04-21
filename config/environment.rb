75d31e57af99f6346ea0fe1938cf03e4dfe93392
Chrispy <pc@matt-schwarz.com>
2010-04-21 20:39:28 +0200

    require original RRDTool Ruby Bindings

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,8 @@
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 COLLECTD_HOME = '/opt/collectd/'
 
+require 'RRD'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
